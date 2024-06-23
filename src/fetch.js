c6370a05da7daee576b888c1d75f340196ef398d
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-06-22 21:38:37 -0400

    search for recipes functionality

--- src/fetch.js
+++ src/fetch.js
@@ -16,3 +16,28 @@ export async function initialFetch() {
     console.error(error);
   }
 }
+export async function searchForRecipe(e) {
+  e.preventDefault();
+
+  try {
+    if (searchRecipeInput.value.length === 0) {
+      throw new Error('Please enter a recipe name');
+    }
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchRecipeInput.value}`
+    );
+    const recipes = await response.json();
+
+    if (recipes.meals === null) {
+      alert('No recipe found! Please try another recipe name.');
+      return;
+    }
+
+    resetDisplay();
+    const shuffledRecipes = shuffle(recipes.meals);
+    shuffledRecipes.forEach((recipe) => displayRecipe(recipe));
+    searchRecipeInput.value = '';
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { initialFetch } from './fetch.js';
+import { initialFetch, searchForRecipe } from './fetch.js';
 
 const searchRecipeForm = document.querySelector('#search-recipe-form');
+
+searchRecipeForm.addEventListener('submit', searchForRecipe);
 
 document.addEventListener('DOMContentLoaded', initialFetch);
